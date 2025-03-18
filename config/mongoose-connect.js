ef17226182bfc7c5a9dd3ec816c00bd769b5c580
Aniket Kumar <aniiket.ak@gmail.com>
2025-03-18 22:58:17 +0530

    db fixes

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@vibhastree.iflzw.mongodb.net/Vibha-Stree`)
+    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@vibhastree.iflzw.mongodb.net/Vibha-Stree`,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+    })
   .then(() => {
     console.log("Database is connected");
   })
