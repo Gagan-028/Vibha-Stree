7eed51b9ae9d3a7109b42a716da7128043816d8d
Aniket Kumar <aniiket.ak@gmail.com>
2025-03-18 22:15:11 +0530

    mongo fixes

--- config/mongoose-connect.js
+++ config/mongoose-connect.js
@@ -2,14 +2,6 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose
   .connect(
-    `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@vibhastree.iflzw.mongodb.net/Vibha-Stree`,{
-      serverSelectionTimeoutMS: 5000,
-    })
-  .then(() => {
-const mongoose = require("mongoose");
-require('dotenv').config();
-mongoose
-  .connect(
     `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@vibhastree.iflzw.mongodb.net/Vibha-Stree`)
   .then(() => {
     console.log("Database is connected");
